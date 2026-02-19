e5670fba78a9fa74b55f6f319a2cdbe50b493cdd
Chris <cristosandoval4@gmail.com>
2026-02-18 18:28:04 -0600

    finished techshell and added to the README file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Shell.c
+
 A Shell program written in C // Luke Turpin and Christopher Sandoval
+
+This program is mimicing how the terminal shell work but making it ourselfs. 
+
+To clone execute like this: git clone https://github.com/lturp05/Shell.c.git
+
+To compile the promgram open integrated terminal and type in: gcc techshell.c -o techshell
+
+To run the program have the integrated terminal and type: ./techshell
--- techshell.c
+++ techshell.c
@@ -2,67 +2,13 @@
 // Description:
 
 
-//////// Some function ideas: ////////////
-// Note: Some code is reflected in main that represents these functions,
-// but it is up to you to determine how you want to organize your code.
-
-/* 
-    A function that causes the prompt to 
-    display in the terminal
-*/
-
-/*
-    A function that takes input from the user.
-    It may return return the input to the calling statement or 
-    store it at some memory location using a pointer.
-*/ 
-
-/*
-    A function that parses through the user input.
-    Consider having this function return a struct that stores vital
-    information about the parsed instruction such as:
-    - The command itself
-    - The arguments that come after the command 
-        Hint: When formatting your data, 
-        look into execvp and how it takes in args.
-    - Information about if a redirect was detected such as >, <, or |
-    - Information about whether or not a new file 
-        needs to be created and what that filename may be.
-    
-
-    Some helpful functions when doing this come from string.h and stdlib.h, such as
-    strtok, strcmp, strcpy, calloc, malloc, realloc, free, and more
-
-    Be sure to consider/test for situations when a backslash is used to escape the space char
-    and when quotes are used to group together various tokens.
-*/
-
-/*
-    A function that executes the command. 
-    This function might take in a struct that represents the shell command.
-
-    Be sure to consider each of the following:
-    1. The execvp() function. 
-        This can execute commands that already exist, that is, 
-        you don't need to recreate the functionality of 
-        the commands on your computer, just the shell.
-        Keep in mind that execvp takes over the current process.
-    2. The fork() function. 
-        This can create a process for execvp to take over.
-    3. cd is not a command like ls and mkdir. 
-        cd is a toold provided by the shell, 
-        so you WILL need to recreate the functionality of cd.
-    4. Be sure to handle standard output redirect and standard input redirects here 
-        That is, there symbols: > and <. 
-        Pipe isn't required but could be a nice addition.
-*/
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <fcntl.h>
-#include <fcntl.h>
+#include <errno.h>
 
 #define MAX_INPUT_SIZE 1024
 #define MAX_ARGS 64
@@ -101,6 +47,8 @@ int main() // MAIN
 	    
 	    // execute the command
 	    executeCommand(command);
+
+        free(input);
 	}
 
 	exit(0);
@@ -112,14 +60,14 @@ void displayPrompt(){
     if (getcwd(cwd, sizeof(cwd)) != NULL) { // Get the current working directory
         printf("%s$ ", cwd); // Display the current working directory followed by a $ symbol
     } else {
-        perror("getcwd() failed"); 
+        printf("error %d: %s\n", errno, strerror(errno)); // Print an error message if getcwd fails
     }
 }
 
 char* getInput(){
     char *buffer = malloc(MAX_INPUT_SIZE); // Allocate memory for the input buffer
     if(!buffer){ // Checks for successful memory allocation
-        perror("malloc() failed");
+        fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
         exit(1);
     }
 
@@ -174,11 +122,11 @@ void executeCommand(struct ShellCommand command){
 
     if(strcmp(command.cmd, "cd") == 0){
         if(command.args[1] == NULL){ // Checks for argument after cd 
-            fprintf(stderr, "cd: missing argument\n"); // 
+            perror("cd: missing argument");
         }
         else{
-            if(chdir(command.args[1]) != 0){ //Change directory and check for errors
-                perror("cd");
+            if(chdir(command.args[1]) != 0){ // Change directory and check for errors
+                fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
             }
         }
         return;
@@ -191,15 +139,15 @@ void executeCommand(struct ShellCommand command){
     pid_t pid = fork(); // creating child process
 
     if(pid < 0){
-        perror("Child process failed");
+        fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
         exit(1);
     }
 
-    if (pid == 0){
+    if(pid == 0){
         if(command.redirectInput){ // Check for input redirection
             int fd = open(command.inputFile, O_RDONLY); // Open the input file for reading and check for errors
             if(fd < 0){
-                perror("open() failed");
+                fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
                 exit(1);
             }
             dup2(fd, STDIN_FILENO); // Redirect standard input to the file descriptor of the opened file
@@ -208,14 +156,15 @@ void executeCommand(struct ShellCommand command){
         if(command.redirectOutput){ // Check for output redirection
             int fd = open(command.outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644); // Open the output file for writing, and create a new one if it doesn't exist, and check for errors
             if(fd < 0){
-                perror("open() failed");
+                fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
                 exit(1);
             }
             dup2(fd, STDOUT_FILENO); // Redirect standard output to the file descriptor of the opened file
             close(fd);
         }
-        execvp(command.cmd, command.args); // Execute the command with arguments and check for erros
-        perror("execvp() failed");
+        errno = 0;
+        execvp(command.cmd, command.args); // Execute the command with arguments and check for errors
+        fprintf(stderr, "error %d: %s\n", errno, strerror(errno));
         exit(1);
     }
     else{
